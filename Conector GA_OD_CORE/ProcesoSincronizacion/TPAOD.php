050eb68cfabf1ce65fba0d9cf3f2d07fd2ee7de9
galcober <galcober@deloitte.es>
2019-11-22 11:39:01 +0100

    Encoding fix

--- Conector GA_OD_CORE/ProcesoSincronizacion/TPAOD.php
+++ Conector GA_OD_CORE/ProcesoSincronizacion/TPAOD.php
@@ -21,16 +21,16 @@ if (empty($VistasActualizar)) {
     // Inicializamos variables vacias para prevenir errores de codigo.
     $ListaVistasActualizar  = '';
     $ParametrosCondicionSQL = '';
-    // Recorremos el array y generamos un string para aï¿½adir a la funcion IN de SQL.
+    // Recorremos el array y generamos un string para añadir a la funcion IN de SQL.
     foreach ($ArrayVistasActualizar as $numArray => $ListaVistasActualizar) {
-        // Si la vista no es del BOA: nos aseguramos que el valor es numerico para evitar SQLi y preparamos la condiciï¿½n.
+        // Si la vista no es del BOA: nos aseguramos que el valor es numerico para evitar SQLi y preparamos la condición.
         if ($ListaVistasActualizar != 'boa_eli') {
             $ParametrosCondicionSQL = $ParametrosCondicionSQL . 'nombre like \'' . filter_var($ListaVistasActualizar, FILTER_SANITIZE_NUMBER_INT) . ' %\' or ';
         } else {
             $ParametrosCondicionSQL = $ParametrosCondicionSQL . 'nombre like \'' . $ListaVistasActualizar . '%\' or ';
         }
     }
-    $ParametrosCondicionSQL      = substr_replace($ParametrosCondicionSQL, "", -3); // Eliminamos la ï¿½ltima coma del string generado.
+    $ParametrosCondicionSQL      = substr_replace($ParametrosCondicionSQL, "", -3); // Eliminamos la ?ltima coma del string generado.
     $ConsultaSQLVistasActualizar = "SELECT distinct code,nombre,criterio,periodicidad,fecha,hora,estado,logs,archivos,active,created FROM public.cargavistas where " . $ParametrosCondicionSQL;
     
 }
